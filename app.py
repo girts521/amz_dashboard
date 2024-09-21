2b0ceecb3c0a86836f9344a4ade5d80832b9db9a
Girts <girts@gkarcevskis.com>
2024-09-21 21:12:06 +0200

    Added query to db for price history

--- app.py
+++ app.py
@@ -2,19 +2,30 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 
+import sqlite3
+
+conn = sqlite3.connect("/home/girts/Documents/scraping/amazon/products.db", check_same_thread=False)
+c = conn.cursor()
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/data', methods=['POST','GET'])
-def handle_data():
-    if request.method == 'POST':
-        data = request.json  # Get JSON data from the request
-        return jsonify({'message': 'Data received!', 'data': data}), 201
-    if request.method == 'GET':
-        df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-        data = df.to_dict(orient='records')
-        return jsonify(data), 201
 
-if __name__ == '__main__':
+@app.route("/data", methods=["GET"])
+def handle_data():
+    if request.method == "GET":
+        product_id = request.args.get('id')
+        print("id: ", product_id)
+        query = f"""
+        SELECT * FROM PriceHistory 
+        WHERE product_id={product_id};
+        """
+        c.execute(query)
+        products = c.fetchall()
+        print("products: ", products)
+        # data = df.to_dict(orient="records")
+        return jsonify(products), 201
+
+
+if __name__ == "__main__":
     app.run(debug=True)
